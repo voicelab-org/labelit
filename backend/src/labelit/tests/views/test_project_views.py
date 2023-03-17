4e9983391b49c1c59a0c50fa6aa3952dd280f5fb
Eric Bolo <bolo.eric@gmail.com>
2023-03-17 14:59:33 +0100

    * DONE fix test errors   * DONE labelit/tests/views/test_project_views.py

--- backend/src/labelit/tests/views/test_project_views.py
+++ backend/src/labelit/tests/views/test_project_views.py
@@ -4,10 +4,13 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIRequestFactory, force_authenticate
 from labelit.models import Project, Dataset, Batch, Document
 from labelit.views import ProjectViewSet
+from labelit.tests import TestSetup
 
 
-class ProjectViewSetTests(TestCase):
+class ProjectViewSetTests(TestSetup, TestCase,):
     def setUp(self):
+
+        super().setUp()
         self.user = get_user_model().objects.create(
             email="tester@labelit",
             password="tester",
@@ -32,12 +35,25 @@ class ProjectViewSetTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_create_project_successful(self):
-        request = self.request_factory.post("", data={"name": "Scriber"})
+        request = self.request_factory.post("", data={
+            "name": "Transcribing meetings",
+            "is_audio_annotated": True,
+            "is_text_annotated": False,
+            "enable_region_annotation": False,
+            "are_sequenced_annotated": False,
+            'tasks': [self.task1.pk],
+            "time_inactivity_threshold": 5000,
+            "do_display_timer_time": False,
+            "does_audio_playing_count_as_activity": True,
+            "target_deadline": "2022-01-03",
+            "target_num_documents": 100,
+            "description": "This project consists in transcribing real-life meetings"
+        })
         self._authenticate(request)
         response = ProjectViewSet.as_view(actions={"post": "create"})(request)
         self.assertEqual(response.status_code, 201)
         projects = Project.objects.all()
-        self.assertEqual(projects.count(), 2)
+        self.assertEqual(projects.count(), 3)
 
     def test_get_remaining_units_in_dataset(self):
         dataset = Dataset.objects.create(name="IMDB")
