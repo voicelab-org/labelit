ec10ce2cb5419b286dea02a20125773b4073c46d
Eric Bolo <bolo.eric@gmail.com>
2023-03-19 12:49:20 +0100

    * DONE make automatic imports DRYer   * CODED models/label_types/__init__.py   * CODED models/task_types/__init__.py

--- backend/src/labelit/models/task_types/__init__.py
+++ backend/src/labelit/models/task_types/__init__.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from labelit.utils.automatically_import_modules import automatically_import_modules
 
-print("globals() before", globals())
-
 package_dir_path = Path(__file__).resolve().parent
 dunder_name = __name__
 automatically_import_modules(
@@ -10,5 +8,3 @@ automatically_import_modules(
     dunder_name,
     globals(),
 )
-
-print("globals() after", globals())
