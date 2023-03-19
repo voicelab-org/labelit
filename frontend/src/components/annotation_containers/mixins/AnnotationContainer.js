dfa81e56d176a276b090824a2c6a66d9e3994164
Eric Bolo <bolo.eric@gmail.com>
2023-03-19 13:16:30 +0100

    Revert "* WIP Enable optional task sequencing"
    This reverts commit e07acc41193722a31de1c9fa3e50ecac4c29a453.

--- frontend/src/components/annotation_containers/mixins/AnnotationContainer.js
+++ frontend/src/components/annotation_containers/mixins/AnnotationContainer.js
@@ -1,30 +1,32 @@
 export default {
-  name: 'AnnotationContainer',
-  props: {
-    document: {
-      type: Object,
-      required: true
+    name: 'AnnotationContainer',
+    props: {
+        document: {
+            type: Object,
+            required: true
+        },
     },
-  },
-  data(){
-    return {}
-  },
-  methods: {
-    getFormForTask(task) {
-      return task.resourcetype + 'Form';
+    data() {
+        return {
+
+        }
     },
-    getTaskForAnnotation(annotation) {
-      var vm = this;
-      return vm.tasks.filter(t => t.id == annotation.task)[0];
+    methods: {
+        getFormForTask(task) {
+            return task.resourcetype + 'Form';
+        },
+        getTaskForAnnotation(annotation) {
+            var vm = this;
+            return vm.tasks.filter(t => t.id == annotation.task)[0];
+        },
+        getAnnotationClasses(a, is_focused) {
+            return {
+                annotation: true,
+                validated: a.has_qa_validated,
+                invalidated: a.has_qa_invalidated,
+                resubmitted: a.is_resubmitted,
+                is_focused: is_focused,
+            };
+        },
     },
-    getAnnotationClasses(a, is_focused) {
-      return {
-        annotation: true,
-        validated: a.has_qa_validated,
-        invalidated: a.has_qa_invalidated,
-        resubmitted: a.is_resubmitted,
-        is_focused: is_focused,
-      };
-    },
-  },
 };
