e546c4abe0a77568716cba85da5cc368a8e32b83
Eric Bolo <bolo.eric@gmail.com>
2023-03-19 14:41:08 +0100

    * WIP prepare serializers for plugin architecture   * WIP automatically register task types in task_serializers/

--- backend/src/labelit/serializers/task_serializer.py
+++ backend/src/labelit/serializers/task_serializer.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from labelit.models import (
     Task,
-    CategoricalTask,
-    OrdinalTask,
-    TranscriptionTask,
-    LiveCorrectTask,
-    EntityTask,
-    TextEditionTask,
-    NestedCategoricalTask,
-    AudioRegionTask,
 )
 from rest_polymorphic.serializers import PolymorphicSerializer
 from .label_serializer import LabelPolymorphicSerializer
+from labelit.serializers.task_serializers import (
+    AudioRegionTaskSerializer,
+    CreateOrUpdateAudioRegionTaskSerializer,
+    CreateOrUpdateCategoricalTaskSerializer,
+    CategoricalTaskSerializer,
+    EntityTaskSerializer,
+    CreateOrUpdateEntityTaskSerializer,
+    LiveCorrectTaskSerializer,
+    NestedCategoricalTaskSerializer,
+    OrdinalTaskSerializer,
+    CreateOrUpdateOrdinalTaskSerializer,
+    TextEditionTaskSerializer,
+    CreateOrUpdateTextEditionTaskSerializer,
+    TranscriptionTaskSerializer,
+    CreateOrUpdateTranscriptionTaskSerializer,
+)
+
 
 # TODO: DRY the code
 
@@ -28,86 +37,21 @@ class CreateOrUpdateTaskSerializer(serializers.ModelSerializer):
         ]
 
 
-class CreateOrUpdateCategoricalTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CategoricalTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "labels",
-            "archived",
-        ]
-
-
-class CreateOrUpdateAudioRegionTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AudioRegionTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "color",
-            "archived",
-        ]
-
-
-class CreateOrUpdateTextEditionTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TextEditionTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "archived",
-        ]
-
-
-class CreateOrUpdateTranscriptionTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TranscriptionTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "archived",
-        ]
-
-
-class CreateOrUpdateOrdinalTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrdinalTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "labels",
-            "archived",
-        ]
-
-
-class CreateOrUpdateEntityTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EntityTask
-        fields = [
-            "id",
-            "name",
-            "can_documents_be_invalidated",
-            "labels",
-            "archived",
-        ]
-
-
 class CreateOrUpdateTaskPolymorphicSerializer(PolymorphicSerializer):
-    model_serializer_mapping = {
-        Task: CreateOrUpdateTaskSerializer,
-        AudioRegionTask: CreateOrUpdateAudioRegionTaskSerializer,
-        CategoricalTask: CreateOrUpdateCategoricalTaskSerializer,
-        OrdinalTask: CreateOrUpdateOrdinalTaskSerializer,
-        EntityTask: CreateOrUpdateEntityTaskSerializer,
-        TextEditionTask: CreateOrUpdateTextEditionTaskSerializer,
-        TranscriptionTask: CreateOrUpdateTranscriptionTaskSerializer,
-    }
+
+    @staticmethod
+    def _get_mapping():
+        return {
+            Task: CreateOrUpdateTaskSerializer,
+            AudioRegionTask: CreateOrUpdateAudioRegionTaskSerializer,
+            CategoricalTask: CreateOrUpdateCategoricalTaskSerializer,
+            OrdinalTask: CreateOrUpdateOrdinalTaskSerializer,
+            EntityTask: CreateOrUpdateEntityTaskSerializer,
+            TextEditionTask: CreateOrUpdateTextEditionTaskSerializer,
+            TranscriptionTask: CreateOrUpdateTranscriptionTaskSerializer,
+        }
+
+    model_serializer_mapping = CreateOrUpdateTaskPolymorphicSerializer._get_mapping()
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -128,189 +72,19 @@ class TaskSerializer(serializers.ModelSerializer):
         ]
 
 
-class CategoricalTaskSerializer(serializers.ModelSerializer):
-    labels = LabelPolymorphicSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = CategoricalTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "are_categories_exclusive",
-            "labels",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class OrdinalTaskSerializer(serializers.ModelSerializer):
-    labels = LabelPolymorphicSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = CategoricalTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class AudioRegionTaskSerializer(serializers.ModelSerializer):
-    labels = serializers.SerializerMethodField("get_labels")
-
-    def get_labels(self, task):
-        labels = task.labels.all()
-        serializer = LabelPolymorphicSerializer(
-            instance=labels, many=True, required=False, read_only=True
-        )
-        return serializer.data
-
-    class Meta:
-        model = AudioRegionTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "color",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class EntityTaskSerializer(serializers.ModelSerializer):
-    labels = serializers.SerializerMethodField("get_labels")
-
-    def get_labels(self, task):
-        labels = task.labels.filter(entitylabel__source_label=None)
-        serializer = LabelPolymorphicSerializer(
-            instance=labels, many=True, required=False, read_only=True
-        )
-        return serializer.data
-
-    class Meta:
-        model = EntityTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class NestedCategoricalTaskSerializer(serializers.ModelSerializer):
-    labels = serializers.SerializerMethodField("get_labels")
-
-    def get_labels(self, task):
-        labels = task.labels.all()
-        serializer = LabelPolymorphicSerializer(
-            instance=labels, many=True, required=False, read_only=True
-        )
-        return serializer.data
-
-    class Meta:
-        model = NestedCategoricalTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class TranscriptionTaskSerializer(serializers.ModelSerializer):
-    labels = LabelPolymorphicSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = TranscriptionTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class TextEditionTaskSerializer(serializers.ModelSerializer):
-    labels = LabelPolymorphicSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = TextEditionTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
-class LiveCorrectTaskSerializer(serializers.ModelSerializer):
-    labels = LabelPolymorphicSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = LiveCorrectTask
-        fields = [
-            "id",
-            "name",
-            "projects",
-            "can_documents_be_invalidated",
-            "labels",
-            "image",
-            "html_guidelines",
-            "archived",
-            "created_at",
-            "updated_at",
-        ]
-
-
 class TaskPolymorphicSerializer(PolymorphicSerializer):
-    model_serializer_mapping = {
-        Task: TaskSerializer,
-        CategoricalTask: CategoricalTaskSerializer,
-        OrdinalTask: OrdinalTaskSerializer,
-        TranscriptionTask: TranscriptionTaskSerializer,
-        LiveCorrectTask: LiveCorrectTaskSerializer,
-        EntityTask: EntityTaskSerializer,
-        TextEditionTask: TextEditionTaskSerializer,
-        NestedCategoricalTask: NestedCategoricalTaskSerializer,
-        AudioRegionTask: AudioRegionTaskSerializer,
-    }
+
+    def _get_mapping(self):
+        return {
+            Task: TaskSerializer,
+            CategoricalTask: CategoricalTaskSerializer,
+            OrdinalTask: OrdinalTaskSerializer,
+            TranscriptionTask: TranscriptionTaskSerializer,
+            LiveCorrectTask: LiveCorrectTaskSerializer,
+            EntityTask: EntityTaskSerializer,
+            TextEditionTask: TextEditionTaskSerializer,
+            NestedCategoricalTask: NestedCategoricalTaskSerializer,
+            AudioRegionTask: AudioRegionTaskSerializer,
+        }
+
+    model_serializer_mapping =TaskPolymorphicSerializer._get_mapping()
