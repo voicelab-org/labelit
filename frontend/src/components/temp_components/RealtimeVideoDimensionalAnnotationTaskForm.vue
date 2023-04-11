6502294e7ef899a90e0cbc97c721e3a37939410b
Eric Bolo <bolo.eric@gmail.com>
2023-04-11 18:11:31 +0200

    * WIP Integrate video into labelit w. use case: THERADIA   * WIP RealtimeVideoDimensionalAnnotationTask     * WIP fe component only       * WIP control video player and register callbacks from task component         * DONE capture videoJS player events         * TODO control player options

---
+++ frontend/src/components/temp_components/RealtimeVideoDimensionalAnnotationTaskForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="task-form-container">RT dimensional annotation goes here !</div>
+</template>
+
+<script>
+import { useVideoPlayer } from '@/composables/video_player.js';
+
+export default {
+  name: 'RealtimeVideoDimensionalAnnotationTaskForm',
+  setup() {
+    // composition API
+    const { player } = useVideoPlayer();
+
+    return {
+      player,
+    };
+  },
+  components: {},
+  mixins: [],
+  mounted() {
+    console.log('this.player.value', this.player.value);
+    this.player.value.on('ended', () => {
+      console.log('&ended event');
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+.task-form-container {
+  margin-bottom: 15px;
+}
+</style>
