154c196a2f1a0ebfde92f9ae5b9091e0066eed23
Eric Bolo <bolo.eric@gmail.com>
2023-04-12 17:56:19 +0200

    * WIP Integrate video into labelit w. use case: THERADIA   * WIP RealtimeVideoDimensionalAnnotationTask     * WIP fe component       * WIP: start, stop tracking based on player state => player is redefined, ready event fired again upon changing playerOptions

--- frontend/src/components/temp_components/RealtimeVideoDimensionalAnnotationTaskForm.vue
+++ frontend/src/components/temp_components/RealtimeVideoDimensionalAnnotationTaskForm.vue
@@ -2,9 +2,19 @@
   <div class="task-form-container">
     RT dimensional annotation goes here !
     <br />
-    <MouseTrackingSlider :do-track="do_start" v-model="position" />
-    <v-btn @click="do_start = true">Start</v-btn>
-    <v-btn @click="do_start = false">Stop</v-btn>
+    <MouseTrackingSlider
+      :do-track="do_track_mouse_position"
+      v-model="position"
+    />
+    <v-btn @click="do_track_mouse_position = true">Start</v-btn>
+    <v-btn @click="do_track_mouse_position = false">Stop</v-btn>
+    <v-btn
+      @click="
+        play();
+        do_track_mouse_position = true;
+      "
+      >Play</v-btn
+    >
   </div>
 </template>
 
@@ -16,16 +26,18 @@ export default {
   name: 'RealtimeVideoDimensionalAnnotationTaskForm',
   setup() {
     // composition API
-    const { player, playerOptions } = useVideoPlayer();
+    const { player, playerOptions, register_player_event_callback } =
+      useVideoPlayer();
 
     return {
+      register_player_event_callback,
       player,
       playerOptions,
     };
   },
   data() {
     return {
-      do_start: false,
+      do_track_mouse_position: false,
       position: 50,
     };
   },
@@ -34,27 +46,81 @@ export default {
   },
   mixins: [],
   mounted() {
-    /*
-    console.log(
-      'this.player.value',
-      JSON.parse(JSON.stringify(this.player.value))
-    );
-    console.log(
-      'player options',
-      JSON.parse(JSON.stringify(this.playerOptions))
-    );
-    this.player.value.on('ended', () => {
-      console.log('&ended event');
+    console.log('&this.player', this.player);
+
+    this.register_player_event_callback('play', () => {
+      console.log('&play from task form!');
     });
+
+    //this.player.on('play', ()=>{console.log("&play!")})
+
+    /*
+      console.log(
+        '&this.player.value',
+        JSON.parse(JSON.stringify(this.player.value))
+      );
+      console.log(
+        'player options',
+        JSON.parse(JSON.stringify(this.playerOptions))
+      );
+      this.player.value.on('ended', () => {
+        console.log('&ended event');
+      });
     */
-    this.playerOptions = {
+
+    /*this.playerOptions = {
       ...this.playerOptions,
       muted: false,
       controls: true,
-    };
-    /*setTimeout(() => {
-      this.player.value.play();
-    }, 300);*/
+    };*/
+
+    /*
+      this.player.value.on('ended', () => {
+        console.log('&ended event');
+        this.do_track_mouse_position = false
+      });
+      this.player.value.on('enterFullWindow', () => {
+        console.log('&enterFullWindow event');
+        //this.do_track_mouse_position = false
+      });
+      this.player.value.on('play', () => {
+        console.log('&play event');
+        //this.do_track_mouse_position = false
+      });
+
+      this.player.value.on('pause', () => {
+        console.log('&pause event');
+        //this.do_track_mouse_position = false
+      });
+     */
+
+    //enterFullWindow
+    /*
+      setTimeout(() => {
+        this.player.value.play();
+      }, 300);
+    */
+  },
+  watch: {
+    position: {
+      handler() {
+        console.log(
+          '&position changed in Realtime...TaskForm: ',
+          this.position
+        );
+      },
+    },
+  },
+  methods: {
+    play() {
+      setTimeout(
+        () => {
+          this.player.currentTime(0);
+          this.player.play();
+        },
+        10 // HACK
+      );
+    },
   },
 };
 </script>
