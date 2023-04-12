c9845637d0cf048e626f1e062cd25f5b05d921ba
Eric Bolo <bolo.eric@gmail.com>
2023-04-12 21:24:06 +0200

    * WIP Integrate video into labelit w. use case: THERADIA   * WIP RealtimeVideoDimensionalAnnotationTask     * WIP fe component       * DONE start, stop tracking based on player state

--- frontend/src/components/temp_components/LabelitVideoPlayer.vue
+++ frontend/src/components/temp_components/LabelitVideoPlayer.vue
@@ -4,9 +4,6 @@
     ref="videoPlayer"
     :options="playerOptions"
     :playsinline="true"
-    @play="onPlayerPlay($event)"
-    @pause="onPlayerPause($event)"
-    @statechanged="playerStateChanged($event)"
     @ready="playerReady"
   >
   </video-player>
@@ -27,56 +24,19 @@ export default {
       playerOptions,
     };
   },
-  mounted() {
-    console.log('this is current player instance object', this.player);
-  },
   computed: {
     computed_player() {
       return this.$refs.videoPlayer.player;
     },
   },
   data() {
-    return {
-      count_ready: 0,
-    };
+    return {};
   },
   methods: {
-    // listen event
-    onPlayerPlay(player) {
-      console.log('player play!', player);
-      console.log('current playback time: ', this.player.currentTime());
-    },
-    onPlayerPause(player) {
-      console.log('player pause!', player);
-    },
-    // ...player event
-
-    // or listen state event
-    playerStateChanged(playerCurrentState) {
-      //console.log('player current update state', playerCurrentState);
-    },
-
     // player is ready
     playerReady(player) {
-      console.log('the player is ready', player, player === this.player);
-
-      this.count_ready++;
-
-      console.log('count ready: ', this.count_ready);
-
-      //if (this.count_ready == 2){ //HACK
       this.set_player(player);
       this.$emit('player-loaded');
-      //}
-
-      /*
-      setTimeout(  // HACK
-          () => {
-            this.$emit('player-loaded');
-          },
-          50,
-      )
-       */
     },
   },
   watch: {},
