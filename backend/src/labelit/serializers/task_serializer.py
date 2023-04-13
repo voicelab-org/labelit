ef28b4cc546a471ecb9cfacf254680ee348c8fcd
Eric Bolo <bolo.eric@gmail.com>
2023-04-13 10:54:24 +0200

    * WIP: refactor backend: group task-specific code inside plugin folders   * DONE serializer mappings     * DRAFTED polymorphic_serializer_mapping_creator     * DRAFTED refactor task_serializer   * DONE import models   * DRAFTED group code

--- backend/src/labelit/serializers/task_serializer.py
+++ backend/src/labelit/serializers/task_serializer.py
@@ -6,11 +6,6 @@ from labelit.services.polymorphic_serializer_mapping_creator import (
     create_polymorphic_serializer_mapping,
 )
 
-# from labelit.serializers.task_types import *
-from zope.dottedname.resolve import resolve
-from django.contrib.contenttypes.models import ContentType
-import os
-
 
 class CreateOrUpdateTaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,71 +19,11 @@ class CreateOrUpdateTaskSerializer(serializers.ModelSerializer):
         ]
 
 
-"""
-def _get_mapping(
-    is_create_or_update=False,
-):
-    def _get_dotted_paths():
-        def _get_task_names():
-            task_model_filenames = list(
-                filter(
-                    lambda fname: fname.endswith("task.py"),
-                    os.listdir(
-                        os.path.join(
-                            os.path.dirname(os.path.abspath(__file__)),
-                            "../models/task_types",
-                        )
-                    ),
-                )
-            )
-
-            def _model_file_name_to_class_name(model_filename):
-                return "".join(
-                    map(
-                        lambda name: name.capitalize(),
-                        model_filename.split(".")[0].split("_"),
-                    )
-                )
-
-            return list(map(_model_file_name_to_class_name, task_model_filenames))
-
-        task_names = _get_task_names()
-
-        return list(map(lambda name: f"labelit.models.{name}", task_names))
-
-    task_dotted_paths = _get_dotted_paths()
-
-    def _get_serializer_dotted_paths():
-        serializer_dotted_paths = []
-        corresponding_task_dotted_paths = []
-        for task_dotted_path in task_dotted_paths:
-            task_name = task_dotted_path.split(".")[-1]
-            if is_create_or_update:
-                dotted_path = f"labelit.task_types.serializers.CreateOrUpdate{task_name}Serializer"
-            else:
-                dotted_path = f"labelit.task_types.serializers.{task_name}Serializer"
-            try:
-                resolve(dotted_path)
-            except ModuleNotFoundError:
-                continue
-            serializer_dotted_paths.append(dotted_path)
-            corresponding_task_dotted_paths.append(task_dotted_paths)
-        return (serializer_dotted_paths, task_dotted_paths)
-
-    serializer_dotted_paths, task_dotted_paths = _get_serializer_dotted_paths()
-
-    return {
-        resolve(task_dotted_path): resolve(serializer_dotted_path)
-        for (serializer_dotted_path, task_dotted_path) in zip(
-            serializer_dotted_paths, task_dotted_paths
-        )
-    }
-"""
-
-
 create_or_update_mappping = create_polymorphic_serializer_mapping(
     is_create_or_update=True
 )
+
+print("&mapping for create/update", create_or_update_mappping)
 
 
 class CreateOrUpdateTaskPolymorphicSerializer(PolymorphicSerializer):
@@ -114,6 +49,8 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 mapping = create_polymorphic_serializer_mapping()
+
+print("&mapping", mapping)
 
 
 class TaskPolymorphicSerializer(PolymorphicSerializer):
--- backend/src/labelit/services/polymorphic_serializer_mapping_creator.py
+++ backend/src/labelit/services/polymorphic_serializer_mapping_creator.py
@@ -13,46 +13,24 @@ def create_polymorphic_serializer_mapping(
         model_type_name = "label"
 
     def _get_dotted_paths_with_corresponding_plugin_names():
-        """
-        def _get_model_names():
-            model_filenames = list(
-                filter(
-                    lambda fname: fname.endswith(f"{model_type_name}.py"),
-                    os.listdir(
-                        os.path.join(
-                            os.path.dirname(os.path.abspath(__file__)),
-                            f"../models/{model_type_name}_types",
-                        )
-                    ),
-                )
-            )
-
-            def _model_file_name_to_class_name(model_filename):
-                return "".join(
-                    map(
-                        lambda name: name.capitalize(),
-                        model_filename.split(".")[0].split("_"),
-                    )
-                )
-
-            return list(map(_model_file_name_to_class_name, model_filenames))
-        """
-
         def _get_model_names():
             def _is_of_desired_type(model_filename):
-                return model_filename.endswith(f"{model_type_name.capitalize()}.py")
+                return model_filename.endswith(f"{model_type_name}.py")
 
             here = os.path.abspath(os.path.dirname(__file__))
             plugins_path = os.path.join(here, "../plugins")
             model_file_names = []
             plugin_names = []
             for plugin_name in os.listdir(plugins_path):
+                if plugin_name.startswith("_"):
+                    continue
                 plugin_model_filenames = list(
                     filter(
                         _is_of_desired_type,
-                        os.listdir(plugins_path, plugin_name, "models"),
+                        os.listdir(os.path.join(plugins_path, plugin_name, "models")),
                     )
                 )
+
                 model_file_names += plugin_model_filenames
                 plugin_names += list(map(lambda _: plugin_name, plugin_model_filenames))
 
@@ -89,14 +67,20 @@ def create_polymorphic_serializer_mapping(
             if is_create_or_update:
                 dotted_path = f"labelit.plugins.{plugin_name}.serializers.CreateOrUpdate{model_name}Serializer"
             else:
-                dotted_path = f"labelit.{model_type_name}_types.serializers.{model_name}Serializer"
+                dotted_path = (
+                    f"labelit.plugins.{plugin_name}.serializers.{model_name}Serializer"
+                )
             try:
                 resolve(dotted_path)
             except ModuleNotFoundError:
+                print(
+                    f"Module not found !! Plugin: {plugin_name}, dotted path: {dotted_path}"
+                )
                 continue
+
             serializer_dotted_paths.append(dotted_path)
-            corresponding_model_dotted_paths.append(model_dotted_paths)
-        return (serializer_dotted_paths, model_dotted_paths)
+            corresponding_model_dotted_paths.append(model_dotted_path)
+        return serializer_dotted_paths, corresponding_model_dotted_paths
 
     serializer_dotted_paths, model_dotted_paths = _get_serializer_dotted_paths()
 
