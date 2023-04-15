be93f3e5081380c19bed3d4b44906ffce11635e7
Eric Bolo <bolo.eric@gmail.com>
2023-04-15 20:47:22 +0200

    * WIP RealtimeVideoDimensionalTask   * WIP annotation flow     * DRAFTED visualize with new LineChart comp.

--- frontend/src/components/LineChart.vue
+++ frontend/src/components/LineChart.vue
@@ -5,31 +5,53 @@
 <script>
 // DataPage.vue
 import { Line } from 'vue-chartjs';
+
 const LineChartjs = Line;
 import {
   Chart,
   Title,
   Tooltip,
   Legend,
-  BarElement,
   CategoryScale,
   LinearScale,
+  PointElement,
+  LineElement,
 } from 'chart.js';
 
-Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+Chart.register(
+  Title,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale
+);
 
 export default {
   name: 'LineChart',
   components: { LineChartjs },
+  props: {
+    x: {
+      type: Array,
+      default: [1, 2, 3],
+    },
+    y: {
+      type: Array,
+      default: [40, 20, 12],
+    },
+  },
   data() {
     return {
       chartData: {
-        labels: ['1', '2', '3'],
+        labels: this.x,
         datasets: [
           {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20, 12],
+            label: 'Annotations',
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+            data: this.y,
           },
         ],
       },
