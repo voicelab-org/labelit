572e9a435ea4c3a89722517fb8e833e73c980133
Eric Bolo <bolo.eric@gmail.com>
2023-04-15 20:14:49 +0200

    * WIP RealtimeVideoDimensionalTask   * WIP annotation flow     * WIP visualize with new LineChart comp.     * DONE record RT annot. sequence

--- frontend/src/components/task_types/RealtimeVideoDimensionalTaskForm.vue
+++ frontend/src/components/task_types/RealtimeVideoDimensionalTaskForm.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="task-form-container">
     <div v-if="step == 0">
-      RT dimensional annotation goes here !
-      <br />
-      Autoplay must be activated in your browser.
-      <br />
-      <MouseTrackingSlider
-        :do-track="do_track_mouse_position"
-        v-model="position"
-      />
-      <v-btn
-        @click="
-          playVideo();
-          do_track_mouse_position = true;
-        "
-      >
-        Play
-      </v-btn>
+      <p>Autoplay must be activated in your browser.</p>
+
+      <p />
+      <div v-if="!is_realtime_sequence_ended">
+        <MouseTrackingSlider
+          :do-track="do_track_mouse_position"
+          v-model="position"
+        />
+        <div class="play-container" v-if="!do_track_mouse_position">
+          <v-btn
+            @click="
+              playVideo();
+              do_track_mouse_position = true;
+            "
+          >
+            Play
+          </v-btn>
+        </div>
+      </div>
+      <div v-if="this.is_realtime_sequence_ended">
+        Sequence: {{ current_realtime_sequence }}
+        <RealtimeSequenceGraph :sequence="current_realtime_sequence" />
+      </div>
     </div>
     <div v-if="step == 1">SUMMATIVE SLIDER</div>
   </div>
@@ -25,6 +32,7 @@
 <script>
 import { useVideoPlayer } from '@/composables/video_player.js';
 import MouseTrackingSlider from '@/components/task_types/components/MouseTrackingSlider.vue';
+import RealtimeSequenceGraph from '@/components/task_types/components/RealtimeSequenceGraph.vue';
 import TaskForm from '@/components/mixins/TaskForm.js';
 
 export default {
@@ -43,10 +51,13 @@ export default {
       do_track_mouse_position: false,
       position: 50,
       step: 0,
+      current_realtime_sequence: [],
+      is_realtime_sequence_ended: false,
     };
   },
   components: {
     MouseTrackingSlider,
+    RealtimeSequenceGraph,
   },
   mixins: [TaskForm],
   props: {
@@ -62,6 +73,10 @@ export default {
   watch: {
     position: {
       handler() {
+        this.current_realtime_sequence.push([
+          this.player.currentTime(),
+          this.position,
+        ]);
         /*console.log(
             '&position changed in Realtime...TaskForm: ',
             this.position
@@ -79,6 +94,7 @@ export default {
     setupEvents() {
       this.player.on('ended', () => {
         this.do_track_mouse_position = false;
+        this.is_realtime_sequence_ended = true;
       });
     },
     playVideo() {
@@ -107,5 +123,10 @@ export default {
 <style lang="scss">
 .task-form-container {
   margin-bottom: 15px;
+
+  .play-container {
+    display: flex;
+    justify-content: center;
+  }
 }
 </style>
