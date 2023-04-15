be71a2e5eeca219bd7c029c7328fe328880c3800
Eric Bolo <bolo.eric@gmail.com>
2023-04-15 20:14:49 +0200

    * WIP RealtimeVideoDimensionalTask   * WIP annotation flow     * WIP visualize with new LineChart comp.     * DONE record RT annot. sequence

---
+++ frontend/src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <LineChartjs :data="chartData" />
+</template>
+
+<script>
+// DataPage.vue
+import { Line } from 'vue-chartjs';
+const LineChartjs = Line;
+import {
+  Chart,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+
+Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default {
+  name: 'LineChart',
+  components: { LineChartjs },
+  data() {
+    return {
+      chartData: {
+        labels: ['1', '2', '3'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12],
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
---
+++ frontend/src/components/task_types/components/RealtimeSequenceGraph.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>RT seq. graph</p>
+    <LineChart />
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart.vue';
+
+export default {
+  name: 'RealtimeSequenceGraph',
+  components: {
+    LineChart,
+  },
+  props: {
+    sequence: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
