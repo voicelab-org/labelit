eebc170a77333896f3031261cbeb967a49e38d8d
Eric Bolo <bolo.eric@gmail.com>
2023-04-17 11:52:33 +0200

    * WIP check fixes for past issues   * DONE /api/batches/274/get_stats/ django.db.utils.DataError: division by zero

--- backend/src/labelit/models/batch.py
+++ backend/src/labelit/models/batch.py
@@ -7,7 +7,10 @@ from .document import Document
 import math
 from django.db.models import Avg, F, ExpressionWrapper, FloatField, Count
 from django.db.models.functions import Cast
+from psycopg2.errors import DivisionByZero
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Batch(PolymorphicModel):
     name = models.CharField("The name of the batch", max_length=200)
@@ -110,18 +113,22 @@ class Batch(PolymorphicModel):
         stats["num_qa_seen"] = stats["num_validated"] + stats["num_invalidated"]
 
         if self.project.is_audio_annotated:
-            stats["average_ratio"] = (
-                done_annotations.values(
-                    "document_id",
+            try:
+                stats["average_ratio"] = (
+                    done_annotations.values(
+                        "document_id",
+                    )
+                    .annotate(
+                        ratio=Cast(F("time"), output_field=FloatField())
+                        / Cast(F("document__audio_duration"), output_field=FloatField()),
+                        time=F("time"),
+                        dur=F("document__audio_duration"),
+                    )
+                    .aggregate(average=Avg(F("ratio")))
                 )
-                .annotate(
-                    ratio=Cast(F("time"), output_field=FloatField())
-                    / Cast(F("document__audio_duration"), output_field=FloatField()),
-                    time=F("time"),
-                    dur=F("document__audio_duration"),
-                )
-                .aggregate(average=Avg(F("ratio")))
-            )
+            except DivisionByZero as e:
+                logger.warning(f"Cannot compute average_ratio, some documents have a duration of 0. {repr(e)}")
+                stats["average_ratio"] = 0
 
             stats["average_duration"] = done_annotations.values(
                 "document_id"
