16c3da973ee204fcb08cc4242b90df75a6932683
Eric Bolo <bolo.eric@gmail.com>
2023-04-27 09:33:48 +0200

    * WIP Plugin folder structure (mirroring backend) in frontend code

---
+++ frontend/src/task_plugins/plugins/ordinal/OrdinalTaskForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="task-form-container">
+    <TaskFormHeader :task="task" :read-only="readOnly" />
+    <LabelSet
+      v-model="selected_labels"
+      :read-only="readOnly"
+      :labels="task.labels"
+      :value-field="'index'"
+    />
+    <ValidationError :message="validationError" />
+  </div>
+</template>
+<script>
+import LabelSet from '@/components/LabelSet.vue';
+import TaskForm from '@/components/mixins/TaskForm.js';
+import ValidationError from '@/components/ValidationError.vue';
+import TaskFormHeader from '@/components/TaskFormHeader.vue';
+
+export default {
+  name: 'OrdinalTaskForm',
+  components: {
+    LabelSet,
+    ValidationError,
+    TaskFormHeader,
+  },
+  mixins: [TaskForm],
+};
+</script>
+
+<style>
+.task-form-container {
+  margin-bottom: 15px;
+}
+</style>
---
+++ frontend/src/task_plugins/plugins/realtime_video_dimensional/components/RealtimeSequenceGraph.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>Your annotations:</p>
+    <div class="realtime-chart-container">
+      <LineChart :x="x" :y="y" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from '@/components/LineChart.vue';
+
+export default {
+  name: 'RealtimeSequenceGraph',
+  components: {
+    LineChart,
+  },
+  props: {
+    sequence: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    x() {
+      return this.sequence.map(x => Math.round(x[0] * 100) / 100);
+    },
+    y() {
+      return this.sequence.map(x => x[1]);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.realtime-chart-container {
+  display: flex;
+  > * {
+    max-height: 300px;
+  }
+}
+</style>
