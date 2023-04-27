55559fc676be669775e5c540e2cf4629258c994b
Eric Bolo <bolo.eric@gmail.com>
2023-04-27 09:59:46 +0200

    * WIP Plugin folder structure (mirroring backend) in frontend code   * DONE refactor: move register_components() outside of main.js

---
+++ frontend/src/utils/automatically_register_components.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+function register_components(files) {
+  for (const fileName in files) {
+    // Get PascalCase name of component
+    const componentName = upperFirst(
+      camelCase(
+        // Gets the file name regardless of folder depth
+        fileName
+          .split('/')
+          .pop()
+          .replace(/\.\w+$/, '')
+      )
+    );
+
+    // Register component
+    Vue.component(componentName, files[fileName].default);
+  }
+}
+
+export default register_components
