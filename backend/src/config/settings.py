a5d829eb2574c3774a105da2ea415aece6904e4d
Wassim Bekalti <wbekalti@batvoice.com>
2025-01-07 13:29:18 +0100

    formatting

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 from datetime import timedelta
@@ -118,7 +119,7 @@ DATABASES = {
         "PORT": 5432,
         "OPTIONS": {
             "options": f"-c search_path={os.environ.get('DB_SCHEMA', 'public')}"
-        }
+        },
     }
 }
 
